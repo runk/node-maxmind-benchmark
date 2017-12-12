9fe1875c76bec387856227621b7333dbb4568ca3
Dmitry Shirokov <deadrunk@gmail.com>
2017-12-13 08:30:41 +1100

    Latest dependencies'

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const randip = () =>
     Math.ceil(Math.random() * 254);
 
 
-const ips = [];
-let ipi = 0;
-for (let i = 0; i < 1e6; i++) { ips.push(randip()); }
-const getip = () => ips[ipi++];
-
 
 const DB_FILE = path.join(__dirname, '/GeoLite2-City.mmdb');
 
@@ -27,34 +22,34 @@ suite.on('cycle', (event) => {
 });
 
 const experiment = (name, fn) => {
-  suite.add(name, { minSamples: 50, fn });
-  ipi = 0;
+  suite.add(name, { minSamples: 100, fn });
 };
 
 
 /******************* maxmind ***********************/
 const maxmind = require('maxmind').openSync(DB_FILE);
-experiment('maxmind', () => { maxmind.get(getip()); });
+experiment('maxmind', () => { maxmind.get(randip()); });
 
 
 /***************** mmdb-reader *********************/
 const mmdbReader = require('mmdb-reader')(DB_FILE);
-experiment('mmdb-reader', () => { mmdbReader.lookup(getip()); });
+experiment('mmdb-reader', () => { mmdbReader.lookup(randip()); });
 
 
 /************* maxmind-db-reader *******************/
 const maxmindDbReader = require('maxmind-db-reader').openSync(DB_FILE);
-experiment('maxmind-db-reader', () => { maxmindDbReader.getGeoDataSync(getip()); });
+experiment('maxmind-db-reader', () => { maxmindDbReader.getGeoDataSync(randip()); });
 
 
 /******************* geoip2  ***********************/
 const geoip2 = require('geoip2').init(DB_FILE);
-experiment('geoip2', () => { geoip2.lookupSync(getip()); });
+experiment('geoip2', () => { geoip2.lookupSync(randip()); });
 
 
 /******************* jgeoip  ***********************/
 const jgeoip = new (require('jgeoip'))(DB_FILE);
-experiment('jgeoip', () => { jgeoip.getRecord(getip()); });
+experiment('jgeoip', () => { jgeoip.getRecord(randip()); });
 
 
+console.log('Benchmarking...');
 suite.run();
